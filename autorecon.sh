b7edb597e09530ed41eee54f88ca786f8ae6b41c
Tae'lur Alexis <taeluralexis@gmail.com>
2023-02-17 02:13:01 -0500

    added amass enum for subdomain enumeration

--- autorecon.sh
+++ autorecon.sh
@@ -12,3 +12,5 @@ $PATH_TO_DIRSEARCH/dirsearch.py -u $DOMAIN -e php -o $DIRECTORY/report.txt
 echo "The results of dirsearch scan are stored in $DIRECTORY/dirsearch."
 gobuster dns -d $DOMAIN -w /usr/share/seclists/Discovery/DNS/subdomains-top1million-5000.txt > $DIRECTORY/subdomains
 echo "The results of gobuster scan are stored in $Directory/subdomains"
+amass enum -w /usr/share/seclists/Discovery/DNS/subddomains-top1million-2000.tx$
+echo "The results of amass scan are stored in $DIRECTORY/amass-enum"
